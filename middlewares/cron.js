7f4305e801187d555506579fb0e29eea159d4e4e
mememelm <carol.arivony@gmail.com>
2022-01-27 00:28:47 +0300

    clean code

--- middlewares/cron.js
+++ middlewares/cron.js
@@ -1,6 +1,5 @@
 const airController = require('../controllers/air-controller')
-// '0 */1 * * * *'
-// '*/10 * * * * *'
+
 var cron = require('cron').CronJob
 var cronJob = new cron('0 */1 * * * *', async () => {
     console.log('c air-paris')
